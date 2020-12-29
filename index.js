37ebfea84f22fb102a8dd60ab01944d712bc699e
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-29 06:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function testFunc() {
 }
 superbowlWin = (record) => {
   let outcome = record.find( record => record.result === "W" )
-  return result ? result.year : undefined
+  return outcome ? outcome.year : undefined
 }
