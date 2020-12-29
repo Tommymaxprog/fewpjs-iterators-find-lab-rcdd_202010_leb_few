c94147979e94637a8e4b473ba7fa82edbcc0228b
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-29 06:41:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+superbowlWin = (record) => {
+  let outcome = record.find( record => record.result === "W" )
+  return result ? result.year : undefined
+}
